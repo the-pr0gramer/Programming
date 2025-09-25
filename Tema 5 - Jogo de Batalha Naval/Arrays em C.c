4caabc69d3c4e54f34c8ce1f6ccc7123f1d5ec3c
the-pr0gramer <fabriciogava12@gmail.com>
2025-09-25 16:48:02 +0000

    Arrays...

---
+++ Tema 5 - Jogo de Batalha Naval/Arrays em C.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 
+int main() {
+    int numeros[5]={10,20,30,40,50};
+
+    printf("O primeiro elemento é %d\n", numeros[0]);
+    printf("O terceiro elemento é %d\n", numeros[2]);
+    printf("O quinto elemento é %d\n", numeros[4]);
+    return 0;
+}
