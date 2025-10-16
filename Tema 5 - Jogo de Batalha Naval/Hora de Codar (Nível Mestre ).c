c16d35a9bc4ba608b0b43480be5878ec3ea2fcec
the-pr0gramer <fabriciogava12@gmail.com>
2025-10-16 00:47:15 +0000

    CodandoOOOO

---
+++ Tema 5 - Jogo de Batalha Naval/Hora de Codar (Nível Mestre ).c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define LINHA 5
+#define COLUNA 5
+
+int main () {
+    int matriz [3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}
+    };
+
+    int target = 5;
+    int found = 0;
+
+    for(int i = 0; i < 3; i++) {
+     for(int j = 0; j < 3; j++) {
+        if (matriz [i][j] == target) {
+            printf("Elemento %d encontrado na posição (%d, %d)\n", target, i, j);
+            found = 1;
+            break;
+        }
+     }
+
+    if (found) break;
+    }
+
+    if (found) {
+        printf("Elemento %d não encontrado na matriz\n", target);
+    }
+
+    return 0;
+}
